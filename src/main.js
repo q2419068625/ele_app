31bd85ff59164c223d5340a4fb745884c48c12c3
q2419068625 <q2419068625@qq.com>
2020-09-07 20:44:29 +0800

    动画轮播和推荐商家

--- src/main.js
+++ src/main.js
@@ -3,8 +3,33 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import axios from 'axios'
+import MintUI from 'mint-ui'
+import 'mint-ui/lib/style.css'
 Vue.config.productionTip = false
+//加载提示框
+import { Indicator } from 'mint-ui';
+
 Vue.prototype.$axios = axios
+
+Vue.use(MintUI)
+
+//请求拦截
+axios.interceptors.request.use(config=>{
+  //加载动画
+  Indicator.open();
+  return config
+},error=>{
+  return Promise.reject(error)
+})
+
+//响应拦截
+axios.interceptors.response.use(response=>{
+  Indicator.close();
+  return response;
+},error=>{
+  Indicator.close();
+  return Promise.reject(error)
+})
 
 new Vue({
   router,
